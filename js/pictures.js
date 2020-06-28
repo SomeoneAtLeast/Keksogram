b6c080ee84771566fb19ae1e728a16a2794806d6
SomeoneAtLeast <winecooler@yandex.ru>
2020-06-29 00:06:59 +0300

    Теперь у каждой фотографии свое фиксированное описание и аватар автора

--- js/pictures.js
+++ js/pictures.js
@@ -23,6 +23,8 @@ function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+let commentFlag = false;
+
 for (let i = 0; i < 25; i++) {
 
     let randomPhoto = Math.floor(Math.random() * photoAddress.length);
@@ -32,36 +34,38 @@ for (let i = 0; i < 25; i++) {
             url: photoAddress[randomPhoto],
             likes: getRandomInRange(15, 200),
             comments: comments[getRandomInRange(0, 5)],
-            description: description[getRandomInRange(0, 5)]
+            description: description[getRandomInRange(0, 5)],
+            src: "img/avatar-" + getRandomInRange(1, 6) + ".svg"
     };
 
     photoAddress.splice(randomPhoto, 1);
 
     let bigPicture = document.querySelector('.big-picture');
 
-    bigPicture.querySelector('.social__header .social__picture').src = "img/avatar-" + getRandomInRange(1, 6) + ".svg";
-
-    
-    let numberOfComments =  getRandomInRange(1, 3);
+    let createsComments = function() {
+        for (let i = 0; i < getRandomInRange(1, 6); i++) {
+            let numberOfComments =  getRandomInRange(1, 3);
         
-    let commentTemplate = document.querySelector('#comment')
-    .content
-    .querySelector('.social__comment');
-
-    let commentElement = commentTemplate.cloneNode(true);
-    commentElement.querySelector('.social__picture').src = "img/avatar-" + getRandomInRange(1, 6) + ".svg";
-    commentElement.querySelector('.social__text').textContent = photo[i].comments;
-    document.querySelector('.social__comments').appendChild(commentElement);
-
-
-    bigPicture.querySelector('.comments-count').textContent = numberOfComments;
-    bigPicture.querySelector('.social__comment-number').textContent = numberOfComments;
+            let commentTemplate = document.querySelector('#comment')
+            .content
+            .querySelector('.social__comment');
+        
+            let commentElement = commentTemplate.cloneNode(true);
+            commentElement.querySelector('.social__picture').src = "img/avatar-" + getRandomInRange(1, 6) + ".svg";
+            commentElement.querySelector('.social__text').textContent = photo[i].comments;
+            document.querySelector('.social__comments').appendChild(commentElement);
+        
+        
+            bigPicture.querySelector('.comments-count').textContent = numberOfComments;
+            bigPicture.querySelector('.social__comment-number').textContent = numberOfComments;
+        };    
+    };
 
     let сlickOnPreview = function() {
     
         bigPicture.classList.remove('hidden');
     
-    
+        bigPicture.querySelector('.social__header .social__picture').src = photo[i].src;
         bigPicture.querySelector('.big-picture__img img').src = photo[i].url;
         bigPicture.querySelector('.likes-count').textContent = photo[i].likes;
         bigPicture.querySelector('.social__caption').textContent = photo[i].description;
@@ -79,6 +83,11 @@ for (let i = 0; i < 25; i++) {
     let photoElement = photoTemplate.cloneNode(true);
     photoElement.addEventListener("click", function(evt) {
         сlickOnPreview();
+        if (!commentFlag) {
+            createsComments();
+
+            commentFlag = true;
+        }
     });
     BigPictureClose.addEventListener("click", function(evt) {
         сlickOnBigPictureClose();
