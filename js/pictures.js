4541a8be90083a71d8f51a023da5f5960e56b43d
SomeoneAtLeast <winecooler@yandex.ru>
2020-07-01 20:52:45 +0300

    Можно закрывать окно редактирования фото, можно менять масштаб фото, можно накладывать фильтры и убирать их, при закрытии окна фильтры и масштаб сбарсываются

--- js/pictures.js
+++ js/pictures.js
@@ -104,11 +104,11 @@ let imgUploadOverlay = document.querySelector('.img-upload__overlay');
 let imgUploadCancel = document.querySelector('.img-upload__cancel');
 
 let openUpload = function () {
-    imgUploadOverlay.classList.remove("hidden");
+    imgUploadOverlay.classList.remove('hidden');
 };
 
 let closeUpload = function () {
-    imgUploadOverlay.classList.add("hidden");
+    imgUploadOverlay.classList.add('hidden');
 };
 
 imgUpload.addEventListener('change', function(evt) {
@@ -117,7 +117,201 @@ imgUpload.addEventListener('change', function(evt) {
 
 imgUploadCancel.addEventListener('click', function() {
     closeUpload();
+    zeroingTheSize();
+    removeEffects();
 });
 
 document.querySelector('.social__comment-count').classList.add('visually-hidden');
 document.querySelector('.social__loadmore').classList.add('visually-hidden');
+
+let uploadPhoto = document.querySelector('.img-upload__preview img');
+let resizeControlMinus = document.querySelector('.resize__control--minus');
+let resizeControlPlus = document.querySelector('.resize__control--plus');
+let resizeControlValue = document.querySelector('.resize__control--value');
+let resizeControlValueNumber = 100;
+
+resizeControlValue.value = resizeControlValueNumber + "%";
+
+let makeASmallerImage = function () {
+    let newResizeControlValue = resizeControlValueNumber - 25;
+    resizeControlValueNumber = newResizeControlValue;
+    resizeControlValue.value = resizeControlValueNumber + "%";
+
+    if (resizeControlValueNumber === 75) {
+        uploadPhoto.classList.add('img-upload__preview--img_75')
+    } else if (resizeControlValueNumber === 50) {
+        uploadPhoto.classList.remove('img-upload__preview--img_75');
+        uploadPhoto.classList.add('img-upload__preview--img_50')
+    } else if (resizeControlValueNumber === 25) {
+        uploadPhoto.classList.remove('img-upload__preview--img_50');
+        uploadPhoto.classList.add('img-upload__preview--img_25')
+    }
+};
+
+let makeABiggerrImage = function () {
+    let newResizeControlValue = resizeControlValueNumber + 25;
+    resizeControlValueNumber = newResizeControlValue;
+    resizeControlValue.value = resizeControlValueNumber + "%";
+
+    if (resizeControlValueNumber === 100) {
+        uploadPhoto.classList.remove('img-upload__preview--img_75')
+    } else if (resizeControlValueNumber === 75) {
+        uploadPhoto.classList.remove('img-upload__preview--img_50')
+        uploadPhoto.classList.add('img-upload__preview--img_75')
+    } else if (resizeControlValueNumber === 50) {
+        uploadPhoto.classList.remove('img-upload__preview--img_25');
+        uploadPhoto.classList.add('img-upload__preview--img_50')
+    }
+};
+
+let zeroingTheSize = function () {
+    resizeControlValueNumber = 100;
+
+    if (uploadPhoto.classList.contains("img-upload__preview--img_75")) {
+        uploadPhoto.classList.remove('img-upload__preview--img_75')
+    } else if (uploadPhoto.classList.contains("img-upload__preview--img_50")) {
+        uploadPhoto.classList.remove('img-upload__preview--img_50')
+    } else if (uploadPhoto.classList.contains("img-upload__preview--img_25")) {
+        uploadPhoto.classList.remove('img-upload__preview--img_25')
+    };
+};
+
+resizeControlMinus.addEventListener('click', function(evt) {
+    if (resizeControlValueNumber != 25) {
+        makeASmallerImage();
+    }
+});
+
+resizeControlPlus.addEventListener('click', function(evt) {
+    if (resizeControlValueNumber != 100) {
+        makeABiggerrImage();
+    }
+});
+
+let noEffect = document.querySelector('.effects__preview--none');
+let chromeEffect = document.querySelector('.effects__preview--chrome');
+let sepiaEffect = document.querySelector('.effects__preview--sepia');
+let marvinEffect = document.querySelector('.effects__preview--marvin');
+let phobosEffect = document.querySelector('.effects__preview--phobos');
+let heatEffect = document.querySelector('.effects__preview--heat');
+
+let removeEffects = function () {
+
+    if (uploadPhoto.classList.contains("effects__preview--sepia")) {
+        uploadPhoto.classList.remove("effects__preview--sepia")
+    } else if (uploadPhoto.classList.contains("effects__preview--marvin")) {
+        uploadPhoto.classList.remove("effects__preview--marvin")
+    } else if (uploadPhoto.classList.contains("effects__preview--phobos")) {
+        uploadPhoto.classList.remove("effects__preview--phobos")
+    } else if (uploadPhoto.classList.contains("effects__preview--heat")) {
+        uploadPhoto.classList.remove("effects__preview--heat")
+    } else if (uploadPhoto.classList.contains("effects__preview--chrome")) {
+        uploadPhoto.classList.remove("effects__preview--chrome")
+    };
+}
+
+
+let applyAnEffectChrom = function () {
+
+    if (uploadPhoto.classList.contains("effects__preview--sepia")) {
+        uploadPhoto.classList.remove("effects__preview--sepia")
+    } else if (uploadPhoto.classList.contains("effects__preview--marvin")) {
+        uploadPhoto.classList.remove("effects__preview--marvin")
+    } else if (uploadPhoto.classList.contains("effects__preview--phobos")) {
+        uploadPhoto.classList.remove("effects__preview--phobos")
+    } else if (uploadPhoto.classList.contains("effects__preview--heat")) {
+        uploadPhoto.classList.remove("effects__preview--heat")
+    };
+
+    uploadPhoto.classList.add('effects__preview--chrome');
+}
+
+let applyAnEffectSepia = function () {
+
+    if (uploadPhoto.classList.contains("effects__preview--chrome")) {
+        uploadPhoto.classList.remove("effects__preview--chrome")
+    } else if (uploadPhoto.classList.contains("effects__preview--marvin")) {
+        uploadPhoto.classList.remove("effects__preview--marvin")
+    } else if (uploadPhoto.classList.contains("effects__preview--phobos")) {
+        uploadPhoto.classList.remove("effects__preview--phobos")
+    } else if (uploadPhoto.classList.contains("effects__preview--heat")) {
+        uploadPhoto.classList.remove("effects__preview--heat")
+    };
+
+    uploadPhoto.classList.add('effects__preview--sepia');
+}
+
+let applyAnEffectMarvin = function () {
+
+    if (uploadPhoto.classList.contains("effects__preview--chrome")) {
+        uploadPhoto.classList.remove("effects__preview--chrome")
+    } else if (uploadPhoto.classList.contains("effects__preview--sepia")) {
+        uploadPhoto.classList.remove("effects__preview--marvin")
+    } else if (uploadPhoto.classList.contains("effects__preview--phobos")) {
+        uploadPhoto.classList.remove("effects__preview--phobos")
+    } else if (uploadPhoto.classList.contains("effects__preview--heat")) {
+        uploadPhoto.classList.remove("effects__preview--heat")
+    };
+
+    uploadPhoto.classList.add('effects__preview--marvin');
+}
+
+let applyAnEffectPhobos = function () {
+
+    if (uploadPhoto.classList.contains("effects__preview--chrome")) {
+        uploadPhoto.classList.remove("effects__preview--chrome")
+    } else if (uploadPhoto.classList.contains("effects__preview--sepia")) {
+        uploadPhoto.classList.remove("effects__preview--sepia")
+    } else if (uploadPhoto.classList.contains("effects__preview--marvin")) {
+        uploadPhoto.classList.remove("effects__preview--marvin")
+    } else if (uploadPhoto.classList.contains("effects__preview--heat")) {
+        uploadPhoto.classList.remove("effects__preview--heat")
+    };
+
+    uploadPhoto.classList.add('effects__preview--phobos');
+}
+
+let applyAnEffectHeat = function () {
+
+    if (uploadPhoto.classList.contains("effects__preview--chrome")) {
+        uploadPhoto.classList.remove("effects__preview--chrome")
+    } else if (uploadPhoto.classList.contains("effects__preview--sepia")) {
+        uploadPhoto.classList.remove("effects__preview--sepia")
+    } else if (uploadPhoto.classList.contains("effects__preview--marvin")) {
+        uploadPhoto.classList.remove("effects__preview--marvin")
+    } else if (uploadPhoto.classList.contains("effects__preview--phobos")) {
+        uploadPhoto.classList.remove("effects__preview--phobos")
+    };
+
+    uploadPhoto.classList.add('effects__preview--heat');
+}
+
+noEffect.addEventListener('click', function(evt) {
+    removeEffects();
+});
+
+chromeEffect.addEventListener('click', function(evt) {
+    applyAnEffectChrom();
+});
+
+sepiaEffect.addEventListener('click', function(evt) {
+    applyAnEffectSepia();
+});
+
+marvinEffect.addEventListener('click', function(evt) {
+    applyAnEffectMarvin();
+});
+
+phobosEffect.addEventListener('click', function(evt) {
+    applyAnEffectPhobos();
+});
+
+heatEffect.addEventListener('click', function(evt) {
+    applyAnEffectHeat();
+});
+
+// let tagField = document.querySelector('.text__hashtags');
+// let tagFieldValue = tagField.value;
+// let tagFieldValueArr = tagFieldValue.split();
+
+// console.log(tagFieldValueArr);
